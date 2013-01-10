2c56909ba1c1e9a31480078317ca9d0096fc6220
johnfitzpatrickrsed1 <john.fitzpatrick+rsed1@rightscale.com>
2013-01-10 15:01:31 +0000

    Update cookbooks/splunk/attributes/default.rb
    Update tar file to splunk-4.3.4-136012-Linux-x86_64.tar

--- cookbooks/splunk/attributes/default.rb
+++ cookbooks/splunk/attributes/default.rb
@@ -1,2 +1,2 @@
 set[:splunk][:installdir]         = "/opt/splunk"
-default[:splunk][:installfile]    = "https://rs-training-assets.s3.amazonaws.com/splunk-5.0-140868-Linux-x86_64.tar"
+default[:splunk][:installfile]    = "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
